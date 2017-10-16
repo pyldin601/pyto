58c45c3092de396a8a9e163eb1ea4fe8f69f0a9b
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2017-10-16 12:42:50 +0300

    Implement base gameplay

--- src/client/components/PytoGame.tsx
+++ src/client/components/PytoGame.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { last } from 'lodash';
+import { last, includes } from 'lodash';
 import Field from './Field';
 import { mod } from '../../shared/util/math';
 
@@ -13,10 +13,11 @@ interface PytoGamePropsInterface {
 interface PytoGameStateInterface {
   player: {
     shake: number[],
-    meat: number,
+    egg: number,
     step: number,
     direction: 'v' | 'h',
   },
+  gameState: 'play' | 'over'
 }
 
 enum KeyCodes {
@@ -32,93 +33,115 @@ export default class PytoGame extends React.Component<PytoGamePropsInterface, Py
 
   constructor(props: PytoGamePropsInterface) {
     super(props);
+
     this.state = {
       player: {
         shake: [0, 1, 2],
-        meat: 30,
+        egg: 30,
         step: 1,
         direction: 'h',
       },
+      gameState: 'play',
     };
     this.square = this.props.width * this.props.height;
 
     this.onKeyPress = this.onKeyPress.bind(this);
   }
 
+  setStepAndDirection(step: number, direction: PytoGameStateInterface['player']['direction']) {
+    if (direction === this.state.player.direction) {
+      // no instant turn
+      return;
+    }
+
+    this.setState({
+      ...this.state,
+      player: { ...this.state.player, step, direction },
+    });
+  }
+
   onKeyPress(event: React.KeyboardEvent<any>) {
     switch (event.which) {
       case KeyCodes.DOWN:
-        this.setState({
-          ...this.state,
-          player: {
-            ...this.state.player,
-            step: this.props.width,
-            direction: 'v',
-          },
-        });
-        break;
+        return this.setStepAndDirection(this.props.width, 'v');
       case KeyCodes.UP:
-        this.setState({
-          ...this.state,
-          player: {
-            ...this.state.player,
-            step: -this.props.width,
-            direction: 'v',
-          },
-        });
-        break;
-      case KeyCodes.LEFT:
-        this.setState({
-          ...this.state,
-          player: {
-            ...this.state.player,
-            step: -1,
-            direction: 'h',
-          },
-        });
-        break;
+        return this.setStepAndDirection(-this.props.width, 'v');
       case KeyCodes.RIGHT:
-        this.setState({
-          ...this.state,
-          player: {
-            ...this.state.player,
-            step: 1,
-            direction: 'h',
-          },
-        });
+        return this.setStepAndDirection(1, 'h');
+      case KeyCodes.LEFT:
+        return this.setStepAndDirection(-1, 'h');
     }
   }
 
   onNextTick() {
-    // is move available
-    // move
-    this.moveShake();
+    if (this.state.gameState !== 'play') {
+      return;
+    }
+
+    const nextPosition = this.calculateNextPosition();
+
+    if (nextPosition === this.state.player.egg) {
+      this.extendShake(nextPosition);
+      return this.placeEgg();
+    }
+
+    if (includes(this.state.player.shake, nextPosition)) {
+      return this.setState({ gameState: 'over' });
+    }
+
+    return this.moveShake(nextPosition);
   }
 
-  moveShake() {
+  calculateNextPosition(): number {
     const shake = this.state.player.shake;
     const step = this.state.player.step;
     const shakeHead = last(shake) as number;
 
     const move = ({
       h: (head: number) => {
-        const verticalOffset = Math.floor(head / this.props.width) * this.props.width;
-        return verticalOffset + mod(head + step, this.props.width);
+        const verticalQuotient = Math.floor(head / this.props.width) * this.props.width;
+        return verticalQuotient + mod(head + step, this.props.width);
       },
       v: (head: number) => {
         return mod(head + step, this.square);
       },
     })[this.state.player.direction];
 
-    const nextPosition = move(shakeHead);
+    return move(shakeHead);
+  }
 
+  moveShake(nextPosition: number) {
+    const shake = this.state.player.shake;
     const newShake = [...shake.slice(1), nextPosition];
+
+    this.setState({
+      ...this.state,
+      player: { ...this.state.player, shake: newShake },
+    });
+  }
+
+  extendShake(nextPosition: number) {
+    const shake = this.state.player.shake;
+    const newShake = [...shake, nextPosition];
+
+    this.setState({
+      ...this.state,
+      player: { ...this.state.player, shake: newShake },
+    });
+  }
+
+  placeEgg() {
+    let pos;
+
+    do {
+      pos = Math.floor(Math.random() * this.square)
+    } while (includes(this.state.player.shake, pos));
 
     this.setState({
       ...this.state,
       player: {
         ...this.state.player,
-        shake: newShake,
+        egg: pos,
       },
     });
   }
@@ -138,7 +161,7 @@ export default class PytoGame extends React.Component<PytoGamePropsInterface, Py
     const lineSize = this.props.width * this.props.height;
     const field = new Array<string>(lineSize).fill('empty');
 
-    field[this.state.player.meat] = 'meat';
+    field[this.state.player.egg] = 'egg';
 
     this.state.player.shake.forEach(part => {
       field[part] = 'shake';
@@ -148,13 +171,24 @@ export default class PytoGame extends React.Component<PytoGamePropsInterface, Py
   }
 
   render() {
+    if (this.state.gameState === 'play') {
+      return (
+        <div tabIndex={0} onKeyDown={this.onKeyPress}>
+          <Field
+            width={this.props.width}
+            height={this.props.height}
+            picture={this.renderPicture()}
+          />
+        </div>
+      );
+    }
+
     return (
-      <div tabIndex={0} onKeyDown={this.onKeyPress}>
-        <Field
-          width={this.props.width}
-          height={this.props.height}
-          picture={this.renderPicture()}
-        />
+      <div>
+
+        <div className="field">
+          <div className="gameover">GAME OVER</div>
+        </div>
       </div>
     );
   }
